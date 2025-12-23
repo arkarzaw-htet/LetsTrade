7d2179a205c121088861f7a705faa4bf7864151e
arkarzaw-htet <arkarzawhtet99@gmail.com>
2025-12-23 12:41:48 +0630

    Change the dom element from array to individuals

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const elements = {
-  BTCUSDT: document.getElementById("btc"),
-  ETHUSDT: document.getElementById("eth"),
-  BNBUSDT: document.getElementById("bnb"),
-  ADAUSDT: document.getElementById("adl"),
-  SOLUSDT: document.getElementById("sol"),
-  XRPUSDT: document.getElementById("xrp"),
-  DOGEUSDT: document.getElementById("doge"),
-  DOTUSDT: document.getElementById("dot"),
-  AVAXUSDT: document.getElementById("ava"),
-  LTCUSDT: document.getElementById("ltc")
-};
+const btc  = document.getElementById("btc");
+const eth  = document.getElementById("eth");
+const bnb  = document.getElementById("bnb");
+const adl  = document.getElementById("adl");
+const sol  = document.getElementById("sol");
+const xrp  = document.getElementById("xrp");
+const doge = document.getElementById("doge");
+const dot  = document.getElementById("dot");
+const ava  = document.getElementById("ava");
+const ltc  = document.getElementById("ltc");
+
 
 const ws = new WebSocket(
   "wss://stream.binance.com:9443/stream?streams=" +
@@ -29,14 +28,20 @@ const ws = new WebSocket(
 
 const prices = {};
 
-ws.onmessage = (event) => {
+ws.onmessage = function (event) {
   const msg = JSON.parse(event.data);
   const d = msg.data;
 
-  prices[d.s] = Number(d.c);
+  prices[d.s] = d.c;
 
-  const el = elements[d.s];
-  if (el) {
-    el.innerText = `${d.s}: ${prices[d.s]}`;
-  }
+  if (d.s === "BTCUSDT") btc.innerText = d.s + ": " + prices[d.s];
+  else if (d.s === "ETHUSDT") eth.innerText = d.s + ": " + prices[d.s];
+  else if (d.s === "BNBUSDT") bnb.innerText = d.s + ": " + prices[d.s];
+  else if (d.s === "ADAUSDT") adl.innerText = d.s + ": " + prices[d.s];
+  else if (d.s === "SOLUSDT") sol.innerText = d.s + ": " + prices[d.s];
+  else if (d.s === "XRPUSDT") xrp.innerText = d.s + ": " + prices[d.s];
+  else if (d.s === "DOGEUSDT") doge.innerText = d.s + ": " + prices[d.s];
+  else if (d.s === "DOTUSDT") dot.innerText = d.s + ": " + prices[d.s];
+  else if (d.s === "AVAXUSDT") ava.innerText = d.s + ": " + prices[d.s];
+  else if (d.s === "LTCUSDT") ltc.innerText = d.s + ": " + prices[d.s];
 };
